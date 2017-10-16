0d144deb725d4c9797745ab1005e1306c2938b40
Javier Martin <Javier.Martin23@alu.uclm.es>
2017-10-16 10:28:11 +0200

    cambio de rama

---
+++ RedSocial/src/modelo/DataBase.java
@@ -0,0 +1,128 @@
+package modelo;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+public class DataBase {
+	MongoClientURI uri;
+	MongoClient client;
+	MongoDatabase db;
+	MongoCollection<Document> dbUsuarios;
+	Document doc, aux;
+	MongoCursor<Document>elementos;
+	
+	public DataBase() {
+		uri  = new MongoClientURI("mongodb://equipo03:pis03equipo@ds113935.mlab.com:13935/equipo03"); 
+        client = new MongoClient(uri);
+	}
+	
+	protected boolean create(Persona p) {
+		try {
+			db = client.getDatabase(uri.getDatabase());
+			dbUsuarios = db.getCollection("usuarios");
+			doc=new Document("email",p.getEmail())
+					.append("clave", p.getPassword())
+					.append("username", p.getUsername())
+					.append("nombre", p.getNombre())
+					.append("apellidos", p.getApellidos())
+					.append("direccion", p.getDireccion())
+					.append("telefono", p.getTelefono())
+					.append("foto", p.getFoto());
+			dbUsuarios.insertOne(doc);
+			return true;
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	
+	protected boolean existeEmail(String email) {
+		boolean existe = false;
+		db = client.getDatabase(uri.getDatabase());
+		dbUsuarios = db.getCollection("usuarios");
+		elementos = dbUsuarios.find().iterator();
+		while(elementos.hasNext()) {
+			doc=elementos.next();
+			if(doc.get("email").toString().equalsIgnoreCase(email))existe=true;
+		}
+		return existe;
+	}
+	
+	protected boolean existeUsername(String username) {
+		boolean existe = false;
+		db = client.getDatabase(uri.getDatabase());
+		dbUsuarios = db.getCollection("usuarios");
+		elementos = dbUsuarios.find().iterator();
+		while(elementos.hasNext()) {
+			doc=elementos.next();
+			if(doc.get("username").toString().equalsIgnoreCase(username))existe=true;
+		}
+		return existe;
+	}
+	
+	protected boolean login(Persona p) {
+		boolean logueado = false;
+		db = client.getDatabase(uri.getDatabase());
+		dbUsuarios = db.getCollection("usuarios");
+		elementos = dbUsuarios.find().iterator();
+		while(elementos.hasNext()) {
+			doc=elementos.next();
+			if((doc.get("email").toString().equalsIgnoreCase(p.getEmail()))&&
+			   (doc.get("clave").toString().equalsIgnoreCase(p.getPassword()))) {
+				logueado=true;
+			}
+		}
+		return logueado;
+	}
+	
+	protected boolean delete(Persona p) {
+		boolean borrado= false;
+		db = client.getDatabase(uri.getDatabase());
+		dbUsuarios = db.getCollection("usuarios");
+		elementos = dbUsuarios.find().iterator();
+		doc=new Document("email",p.getEmail())
+				.append("clave", p.getPassword())
+				.append("username", p.getUsername())
+				.append("nombre", p.getNombre())
+				.append("apellidos", p.getApellidos())
+				.append("direccion", p.getDireccion())
+				.append("telefono", p.getTelefono())
+				.append("foto", p.getFoto());
+		while(elementos.hasNext()) {
+			aux=elementos.next();
+			if((aux.get("email").toString().equalsIgnoreCase(p.getEmail()))&&
+			   (aux.get("clave").toString().equalsIgnoreCase(p.getPassword()))) {
+				dbUsuarios.deleteOne(doc);
+				borrado=true;
+			}
+		}
+		return borrado;
+	}
+	protected boolean update(Persona p){
+		boolean actualizado=false;
+		db = client.getDatabase(uri.getDatabase());
+		dbUsuarios = db.getCollection("usuarios");
+		elementos = dbUsuarios.find().iterator();
+		doc=new Document("email",p.getEmail())
+				.append("clave", p.getPassword())
+				.append("username", p.getUsername())
+				.append("nombre", p.getNombre())
+				.append("apellidos", p.getApellidos())
+				.append("direccion", p.getDireccion())
+				.append("telefono", p.getTelefono())
+				.append("foto", p.getFoto());
+		while(elementos.hasNext()) {
+			aux=elementos.next();
+			if((aux.get("email").toString().equalsIgnoreCase(p.getEmail()))) {
+				dbUsuarios.updateOne(aux,doc);
+				actualizado=true;
+			}
+		}
+		return actualizado;
+	}
+}
