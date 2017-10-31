332b6a20ba311ad9f0c9205c3690b8b4d36d72ab
Antonio <antonio.garcialabarrera@alu.uclm.es>
2017-10-31 22:34:02 +0100

    Servlets CRUD

---
+++ RedSocial/src/main/java/com/pis/redSocial/DeleteUserController.java
@@ -0,0 +1,34 @@
+package com.pis.redSocial;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.DAOPersona;
+import modelo.Persona;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class DeleteUserController {
+
+	@RequestMapping("borrarUsuario")
+	public ModelAndView borrarUser(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+		//logger.info("Register page! The client locale is {}.", locale);
+		String email;
+		email = request.getParameter("inputEmailRegistro");
+		DAOPersona dao = new DAOPersona();
+		if(!dao.existeEmail(email)){
+			//Mostrar Error NO Existe Mail
+		}else{
+				
+				dao.deleteEmail(email);
+		}
+		return new ModelAndView("home");
+	}
+}
---
+++ RedSocial/src/main/java/com/pis/redSocial/UpdateUserController.java
@@ -0,0 +1,47 @@
+package com.pis.redSocial;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.DAOPersona;
+import modelo.Persona;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class UpdateUserController {
+
+	@RequestMapping("modificarUsuario")
+	public ModelAndView updateUser(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+		//logger.info("Register page! The client locale is {}.", locale);
+		String nombre, apellidos, username, email, password, direccion, telefono;
+		username = request.getParameter("inputUsernameRegistro");
+		email = request.getParameter("inputEmailRegistro");
+		DAOPersona dao = new DAOPersona();
+		Persona p;
+		if(dao.existeUsername(username)){
+			//Mostrar Error Existe Usuario
+		}else{
+			if(dao.existeEmail(email)){
+				//Mostrar Error Existe Email
+			}else {
+				//CREA USUARIO
+				nombre=request.getParameter("inputNombreRegistro");
+				apellidos=request.getParameter("inputApellidosRegistro");
+				password=request.getParameter("inputPasswordRegistro");
+				direccion=request.getParameter("inputDireccionRegistro");
+				telefono=request.getParameter("inputTelefonoRegistro");
+				p = new Persona(nombre, apellidos, username, email, password, direccion, telefono, "", false);
+				
+				dao.update(p);
+			}	
+		}
+		return new ModelAndView("home");
+	}
+}
