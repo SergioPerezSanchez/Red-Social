35bcf7bf4cd515e9e2b36a0c6038798cdeeb2fc3
UlisesCeca2 <Ulises.Ceca@alu.uclm.es>
2017-11-30 13:52:50 +0100

    cambiado captcha para que funcione en localhost

--- src/main/java/modelo/VerifyCaptch.java
+++ src/main/java/modelo/VerifyCaptch.java
@@ -15,7 +15,7 @@ import javax.net.ssl.HttpsURLConnection;
 public class VerifyCaptch {
 
 	public static final String url = "https://www.google.com/recaptcha/api/siteverify";
-	public static final String secret = "6Let7joUAAAAAPna1dSBngkUor5p5klo8Fx6Gsh2";
+	public static final String secret = "6Ld6GTsUAAAAADw5m0bxmegTeSWTf2Pxd8rjpvY6";
 	private final static String USER_AGENT = "Mozilla/5.0";
 
 	public static boolean verify(String gRecaptchaResponse) throws IOException {
